16d232495ccb2a45df006c0293c4c0e791f07819
Carnagiul-42 <piquerue@student.42.fr>
2018-08-07 05:16:21 +0200

    update extenstion get (security Breach)

--- libft/ft_gnl/ft_getextension.c
+++ libft/ft_gnl/ft_getextension.c
@@ -6,7 +6,7 @@
 /*   By: piquerue <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/05 20:05:08 by piquerue          #+#    #+#             */
-/*   Updated: 2017/07/17 03:37:55 by piquerue         ###   ########.fr       */
+/*   Updated: 2018/08/07 05:07:26 by piquerue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*ft_get_extension(char *str)
 	char	*ret;
 
 	i = 0;
+	if (ft_strlen(str) <= 2)
+		return (NULL);
 	tab = ft_strsplit(str, '.');
 	while (tab[i])
 		i++;
