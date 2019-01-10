a423dad3414f6dbe71350ee79f96aa5b01989933
k1k1 <pierre.querue@ebg.net>
2019-01-10 12:21:19 +0100

    Adding new function on lib math

--- Include/ft_math.h
+++ Include/ft_math.h
@@ -6,20 +6,23 @@
 /*   By: piquerue <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/18 19:55:40 by piquerue          #+#    #+#             */
-/*   Updated: 2017/05/17 23:33:59 by piquerue         ###   ########.fr       */
+/*   Updated: 2019/01/10 12:20:04 by piquerue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_MATH_H
 # define FT_MATH_H
 
-int			ft_pow(int nb);
-int			ft_math_sum(int a, int b);
-int			ft_math_factorial(int nb);
-int			ft_math_mod(int a, int div);
-int			ft_math_div(int a, int div);
-int			ft_math_abs(int nb);
-long long	ft_pow_l(long long nbr);
-long		ft_power(long nb, size_t pw);
+int					ft_pow(int nb);
+int					ft_math_sum(int a, int b);
+int					ft_math_factorial(int nb);
+int					ft_math_mod(int a, int div);
+int					ft_math_div(int a, int div);
+int					ft_math_abs(int nb);
+long long int		ft_math_lli_abs(long long int nb)
+long int			ft_math_lli_abs(long int nb)
+long long			ft_pow_l(long long nbr);
+long				ft_power(long nb, size_t pw);
+int					ft_math_factoriel(int i)
 
 #endif
